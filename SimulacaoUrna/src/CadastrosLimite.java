58737e95a09267bc5b114f9e737c748a9b2834c6
kkaldas <cainacc@hotmail.com>
2018-05-12 21:33:09 -0300

    Atualizei UML
    estou fazendo na sequencia de metodos a classe CadastrosLimite. Estou no povoaArrayCandidatos...

---
+++ SimulacaoUrna/src/CadastrosLimite.java
@@ -0,0 +1,8 @@
+
+public class CadastrosLimite {
+	public void boasVindas() {
+		System.out.println("Bem vindx aa simulacao de urna. Vamos comecar simulando o primeiro turno de uma votacao. Por favor, cadastre os candidatos.");
+	}
+	
+	
+}
