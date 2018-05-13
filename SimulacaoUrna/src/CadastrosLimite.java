012fae6c12336fe31ae29d1b4b8c8c8f6de2cb78
kkaldas <cainacc@hotmail.com>
2018-05-13 12:17:50 -0300

    Implementei tudo que esta no CadastrosLimite
    Exceto o alteraZonas.
    To testando nesse momento

--- SimulacaoUrna/src/CadastrosLimite.java
+++ SimulacaoUrna/src/CadastrosLimite.java
@@ -19,6 +19,7 @@ public class CadastrosLimite {
 			System.out.println("1: cadastrar candidato.");
 			System.out.println("2: remover candidato.");
 			System.out.println("-1: Encerrar");
+			flag = scanner.nextInt();
 			switch(flag) {
 				case 1:
 					
@@ -79,6 +80,7 @@ public class CadastrosLimite {
 			System.out.println("1: cadastrar candidato.");
 			System.out.println("2: remover candidato.");
 			System.out.println("-1: Encerrar");
+			flag = scanner.nextInt();
 			switch(flag) {
 				case 1:
 					
@@ -160,7 +162,7 @@ public class CadastrosLimite {
 		Scanner scanner = new Scanner(System.in);
 		for(int i=0; i<geralControlador.getVotacoes().size() ; i++) {
 			for(int j=0; j<geralControlador.getVotacoes().get(i).getZonasEleitorais().size(); j++) {
-				System.out.println("Para a votacao " + i + ", Sessao "+geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getZonaEleitoral()
+				System.out.println("Para a votacao " + i + ", Zona "+geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getZonaEleitoral()
 						+ ", quantas sessoes serao cadastradas?");
 				int qtddSessoes = scanner.nextInt();
 				for ( int k =0; k<qtddSessoes; k++) {
@@ -168,45 +170,224 @@ public class CadastrosLimite {
 					int qtddEleitores = scanner.nextInt();
 					UrnaEntidade urna = new UrnaEntidade(qtddEleitores, "primeiro", candGov, candDep );
 					System.out.println("A urna foi carregada com os candidatos previamente cadastrados");
-					System.out.println("Digite o nome da sessao");
+					System.out.println("Digite o nome da secao");
 					String nome = scanner.next();
 					SessaoEntidade sessao = new SessaoEntidade(nome, urna);
+					ArrayList<SessaoEntidade> sessoes = geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getSessoes();
+					sessoes.add(sessao);
+					geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).setSessoes(sessoes);
 				}
 			}
 		}
 	}
 	
-	public void exibeOpcoesAlteracao(GeralControlador2 geralControlador) {
+	public int exibeOpcoesAlteracao() {
 		Scanner scanner = new Scanner(System.in);
 		int flag = 0;
-		while (flag!= -1) {
 			System.out.println("Momento das alteracoes. Escolha uma das opcoes:");
-			System.out.println("1: Adicionar Candidato");
-			System.out.println("2: Remover Candidato");
-			System.out.println("3: Adicionar Sessao");
-			System.out.println("4: Remover Sessao");
+			System.out.println("1: Alterar Candidatos a Governador");
+			System.out.println("2: Alterar Candidatos a Deputado");
+			System.out.println("3: Alterar Secoes");
 			System.out.println("-1: Estou satisfeitx, quero prosseguir.");
-			switch(flag) {
+			flag = scanner.nextInt();
+			return flag;
+	}
+	
+	public void alteraCandidatosGovernador(GeralControlador2 geralControlador) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<CandidatoEntidade> candidatos = 
+				geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0).getSessoes().get(0).getUrna().getCandidatosGovernador();
+		System.out.println("Alteracao de Cadastro de candidatos para Governador.");
+		int flag2 = 0;
+		do {
+			System.out.println("Entre com uma das opcoes: ");
+			System.out.println("1: adicionar candidato.");
+			System.out.println("2: remover candidato.");
+			System.out.println("-1: Encerrar");
+			switch(flag2) {
 				case 1:
-					for(int i=0; i<geralControlador.getVotacoes().size() ; i++) {
-						for(int j=0; j<geralControlador.getVotacoes().get(i).getZonasEleitorais().size(); j++) {
+					
+					System.out.println("Digite o nome do candidato");
+					String nome = scanner.next();
+					
+					System.out.println("Entre com o numero do Partido");
+					PartidoEnum ap[] = PartidoEnum.values(); //sigla para Array de Partidos
+					
+					for (int i = 0; i<ap.length; i++) {
+						System.out.println(i + ": " + ap[i]);
+					}
+					PartidoEnum partido = ap[scanner.nextInt()];
+					
+					System.out.println("Entre com o numero do candidato, de 1 a 98");
+					int numero = scanner.nextInt();
+					
+					CandidatoEntidade cand = this.candidatoControlador.InstanciarCandidatoGovernador(nome, partido, numero);
+					cand.setCargo(CargoEnum.GOVERNADOR);
+					
+					candidatos.add(cand);
+					break;
+					
+				case 2:
+					System.out.println("Favor entrar com o nome do candidato");
+					String name = scanner.next();
+					boolean removido = false;
+					for(int i =0; i<candidatos.size(); i++) {
+						CandidatoEntidade candidatinho = candidatos.get(i);
+						if(name == candidatinho.getNome()) {
+							candidatos.remove(i);
+							removido=true;
 						}
 					}
-						
+					if(removido) {
+						System.out.println("Candidato removido com sucesso");
+					} else {
+						System.out.println("Nao consta um candidato com esse nome na lista");
+					}
 					
-					break;
-				case 2:
-					break;
-				case 3:
-					break;
-				case 4:
 					break;
 				case -1:
 					break;
 				default:
 					break;
 			}
+		}while (flag2!= -1);
+		
+		for(int i=0; i<geralControlador.getVotacoes().size() ; i++) {
+			for(int j=0; j<geralControlador.getVotacoes().get(i).getZonasEleitorais().size(); j++) {
+				for (int k = 0; k<geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getSessoes().size();k++){
+geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getSessoes().get(k).getUrna().setCandidatosGovernador(candidatos);
+				}
+			}
 		}
+	}
+	
+	public void alteraCandidatosDeputado(GeralControlador2 geralControlador) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<CandidatoEntidade> candidatos = 
+				geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0).getSessoes().get(0).getUrna().getCandidatosDeputado();
+		System.out.println("Alteracao de Cadastro de candidatos para Deputado.");
+		int flag2 = 0;
+		do {
+			System.out.println("Entre com uma das opcoes: ");
+			System.out.println("1: adicionar candidato.");
+			System.out.println("2: remover candidato.");
+			System.out.println("-1: Encerrar");
+			switch(flag2) {
+				case 1:
+					
+					System.out.println("Digite o nome do candidato");
+					String nome = scanner.next();
+					
+					System.out.println("Entre com o numero do Partido");
+					PartidoEnum ap[] = PartidoEnum.values(); //sigla para Array de Partidos
+					
+					for (int i = 0; i<ap.length; i++) {
+						System.out.println(i + ": " + ap[i]);
+					}
+					PartidoEnum partido = ap[scanner.nextInt()];
+					
+					System.out.println("Entre com o numero do candidato, de 1 a 98");
+					int numero = scanner.nextInt();
+					
+					CandidatoEntidade cand = this.candidatoControlador.InstanciarCandidatoDeputado(nome, partido, numero);
+					cand.setCargo(CargoEnum.DEPUTADO);
+					
+					candidatos.add(cand);
+					break;
+					
+				case 2:
+					System.out.println("Favor entrar com o nome do candidato");
+					String name = scanner.next();
+					boolean removido = false;
+					for(int i =0; i<candidatos.size(); i++) {
+						CandidatoEntidade candidatinho = candidatos.get(i);
+						if(name == candidatinho.getNome()) {
+							candidatos.remove(i);
+							removido=true;
+						}
+					}
+					if(removido) {
+						System.out.println("Candidato removido com sucesso");
+					} else {
+						System.out.println("Nao consta um candidato com esse nome na lista");
+					}
+					
+					break;
+				case -1:
+					break;
+				default:
+					break;
+			}
+		}while (flag2!= -1);
+		
+		for(int i=0; i<geralControlador.getVotacoes().size() ; i++) {
+			for(int j=0; j<geralControlador.getVotacoes().get(i).getZonasEleitorais().size(); j++) {
+				for (int k = 0; k<geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getSessoes().size();k++){
+geralControlador.getVotacoes().get(i).getZonasEleitorais().get(j).getSessoes().get(k).getUrna().setCandidatosDeputado(candidatos);
+				}
+			}
+		}
+	}
+	
+	public void alteraSessoes(GeralControlador2 geralControlador) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<SessaoEntidade> sessoes = 
+				geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0).getSessoes();
+			int flag2 = 0;
+			do {
+				System.out.println("Entre com uma das opcoes: ");
+				System.out.println("1: adicionar secao.");
+				System.out.println("2: remover secao.");
+				System.out.println("-1: Encerrar");
+				switch(flag2) {
+					case 1:
+						System.out.println("Cada secao tem uma urna, entao entre com os dados da urna");
+						System.out.println("Digite o numero de eleitores para a urna");
+						int numEleit = scanner.nextInt();
+						UrnaEntidade urna = new UrnaEntidade(numEleit, "primeiro",sessoes.get(0).getUrna().getCandidatosGovernador(),
+								sessoes.get(0).getUrna().getCandidatosDeputado());
+						System.out.println("Agora informe o nome da secao");
+						String nome = scanner.next();
+						SessaoEntidade sessao = new SessaoEntidade(nome, urna);
+						System.out.println("Digite o nome da zona eleitoral aa qual essa secao vai pertencer");
+						String nomeZona = scanner.next();
+						System.out.println("A qual votacao pertence essa zona? Digitar numero da votacao.");
+						int vot = scanner.nextInt();
+						for (ZonaEleitoralEntidade zona : geralControlador.getVotacoes().get(vot).getZonasEleitorais()) {
+							if(zona.getZonaEleitoral() == nomeZona) {
+								ArrayList<SessaoEntidade> sessoesEleit = zona.getSessoes();
+								sessoesEleit.add(sessao);
+								zona.setSessoes(sessoesEleit);
+								System.out.println("Secao adicionada!");
+							}
+						}
+						break;
+					case 2:
+						System.out.println("De qual Zona Eleitoral desejas remover uma secao?");
+						String nomeZona2 = scanner.next();
+						System.out.println("A qual votacao pertence essa zona? Digitar numero da votacao.");
+						int vot2 = scanner.nextInt();
+						System.out.println("Digite o nome da secao");
+						String nomeSessao = scanner.next();
+						for (ZonaEleitoralEntidade zona : geralControlador.getVotacoes().get(vot2).getZonasEleitorais()) {
+							if(zona.getZonaEleitoral() == nomeZona2) {
+								ArrayList<SessaoEntidade> sessoesEleit = zona.getSessoes();
+								for(SessaoEntidade sessao2 : sessoesEleit) {
+									if(nomeSessao == sessao2.getSessao()) {
+										sessoesEleit.remove(sessao2);
+										zona.setSessoes(sessoesEleit);
+										System.out.println("Secao removida!");
+									}
+								}
+							}
+						}
+						break;
+					case -1:
+						break;
+					default:
+						break;
+				}
+			} while(flag2!=-1);
 	}
 	
 	/*public UrnaEntidade retornaUrna (ArrayList<CandidatoEntidade> candGov, ArrayList<CandidatoEntidade> candDep) {
--- SimulacaoUrna/src/GeralControlador2.java
+++ SimulacaoUrna/src/GeralControlador2.java
@@ -36,13 +36,43 @@ public class GeralControlador2 {
 	public static void main (String args[]) {
 		GeralControlador2 geralControlador = new GeralControlador2();
 		CadastrosLimite cadastrosLimite = new CadastrosLimite();
+		VotoControlador votoControlador = new VotoControlador();
+		PrimeiroTurnoControlador primeiroTurnoControlador = new PrimeiroTurnoControlador();
 		cadastrosLimite.boasVindas();
 		//geralControlador.cadastraCoisas();
 		cadastrosLimite.povoaVotacoes(geralControlador);
+		System.out.println(geralControlador.getVotacoes().get(0));
 		cadastrosLimite.povoaZonas(geralControlador);
+		System.out.println(geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0));
 		ArrayList<CandidatoEntidade> candGov = cadastrosLimite.povoaArrayCandidatosGovernador();
+		System.out.println(candGov.get(0));
 		ArrayList<CandidatoEntidade> candDep = cadastrosLimite.povoaArrayCandidatosDeputado();
+		System.out.println(candDep.get(0));
 		cadastrosLimite.povoaSessoes(geralControlador, candGov, candDep);
-		
+		System.out.println(geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0).getSessoes().get(0));
+		System.out.println(geralControlador.getVotacoes().get(0).getZonasEleitorais().get(0).getSessoes().get(0).getUrna());
+		int flag = cadastrosLimite.exibeOpcoesAlteracao();
+		while (flag!= -1) {
+		switch(flag) {
+			case 1:
+					cadastrosLimite.alteraCandidatosGovernador(geralControlador);
+				break;
+			case 2:
+					cadastrosLimite.alteraCandidatosDeputado(geralControlador);
+				break;
+			case 3:
+					cadastrosLimite.alteraSessoes(geralControlador);
+				break;
+			case -1:
+				break;
+			default:
+				break;
+		}
+	}
+		votoControlador.instanciarVoto(1, 1, geralControlador.votacoes.get(0).getZonasEleitorais().get(0).getSessoes().get(0).getUrna());
+		votoControlador.instanciarVoto(1, 1, geralControlador.votacoes.get(0).getZonasEleitorais().get(0).getSessoes().get(0).getUrna());
+		primeiroTurnoControlador.listarVotosDeTodasAsUrnas(geralControlador.votacoes.get(0));
+		primeiroTurnoControlador.gerarMapaVotosGovernador(geralControlador.votacoes.get(0));
+		System.out.println(primeiroTurnoControlador.definirGovernadorVencedor(geralControlador.votacoes.get(0)).getNome());
 	}
 }
